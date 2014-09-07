5190bb7cc776f0a8e98a0073577726442a0157a2
Pelle Wessman <pelle@kodfabrik.se>
2014-09-07 14:46:18 +0100

    Initial build of indie-action component

--- src/main.js
+++ src/main.js
@@ -1,17 +1,65 @@
 (function(){  
 
-  // Create your component here
-  // http://x-tags.org/docs
-  
-  xtag.register('x-stub', {
+  var indieConfig = undefined,
+    indieConfigFrame = undefined,
+    loadIndieConfig = function () {
+      if (indieConfigFrame) {
+        return;
+      }
+
+      indieConfigFrame = document.createElement('iframe');
+      indieConfigFrame.src = 'web+indieconfig:load';
+      document.getElementsByTagName('body')[0].appendChild(indieConfigFrame);
+
+      window.addEventListener('message', parseIndieConfig);
+    },
+    parseIndieConfig = function (message) {
+      var correctSource = (indieConfigFrame && message.source === indieConfigFrame.contentWindow);
+
+      if (!correctSource) {
+        return;
+      }
+
+      try {
+        indieConfig = JSON.parse(message.data);
+      } catch (e) {}
+
+      if (indieConfig) {
+        indieConfigFrame.parentNode.removeChild(indieConfigFrame);
+        indieConfigFrame = undefined;
+        xtag.fireEvent(window, 'IndieConfigLoaded');
+        window.removeEventListener('message', parseIndieConfig)
+      }
+    };
+
+  xtag.register('indie-action', {
+    extends: 'a',
     lifecycle: {
-      created: function() {},
+      created: function() {
+        this.innerHTML = '<a href="#">An Action!</a';
+      },
       inserted: function() {},
       removed: function() {},
       attributeChanged: function() {}
     }, 
     events: { 
-    
+      'click:delegate(a)' : function (e) {
+        e.preventDefault();
+        var self = this;
+        var doTheAction = function () {
+          window.location = indieConfig.reply + '?url=' + encodeURIComponent(window.location.href);
+        };
+        var waitForConfig = function () {
+          window.removeEventListener('IndieConfigLoaded', waitForConfig);
+          doTheAction(self);
+        };
+        if (!indieConfig) {
+          window.addEventListener('IndieConfigLoaded', waitForConfig);
+          loadIndieConfig();
+        } else {
+          doTheAction(self);
+        }
+      }
     },
     accessors: {
       
